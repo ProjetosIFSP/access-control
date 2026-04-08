e007e2f011491deb516e1d872fd5a200328e3e38
abnerjs <abner.js05@gmail.com>
2026-04-08 14:01:16 -0300

    cleanup: ai files

--- test-better-auth.mjs
+++
@@ -1,3 +0,0 @@
-import { createAuthClient } from "better-auth/client";
-const authClient = createAuthClient();
-console.log(Object.keys(authClient).filter(k => k.toLowerCase().includes('password')));
